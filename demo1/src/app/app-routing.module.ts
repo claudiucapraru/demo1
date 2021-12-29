24db58ef463e56a678f6c70a106ade17bcd105cc
claudiucapraru <claudiucapraru37@gmail.com>
2021-12-29 11:05:59 +0200

    Routin; home,search components

--- demo1/src/app/app-routing.module.ts
+++ demo1/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  { path: "home", component: HomeComponent, pathMatch: "full" },
+  { path: "**", redirectTo: "home" },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
